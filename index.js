8374ce74d4e304fb02f688373c98007912829e15
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-03 18:30:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 function printBadges(arr) {
   for (let i = 0; i < arr.length; i++) {
-    console.log(`Welcome ${employee[i]}! You are employee #${i + 1}.`);
+    console.log(`Welcome ${arr[i]}! You are employee #${i + 1}.`);
   }
   return arr;
 }
